4c3d98c95d8a1ef9d76a81208ca5cb265a8cde1d
Felipe Tavares <xfelipetavares@gmail.com>
2022-11-26 17:16:48 -0300

    typescript converted

---
+++ src/components/SolarSystem.tsx
@@ -0,0 +1,13 @@
+import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+export default function SolarSystem() {
+  const result = planets.map((planet) => <PlanetCard key={planet.name} planetName={planet.name} planetImage={planet.image} />);
+  return (
+    <div data-testid="solar-system">
+      <Title headline="Planetas" />
+      {result}
+    </div>
+  );
+}
