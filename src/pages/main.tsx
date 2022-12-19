c25e482397069d7a8d448706edecec94f9379cfc
Felipe Tavares <xfelipetavares@gmail.com>
2022-12-19 07:18:35 -0300

    fix bug main file

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '../styles/global.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
